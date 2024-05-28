85f0ffeb60529e2fd7e3c50788b681c6a78dda54
Jefter Xavier <marcojefter@gmail.com>
2024-05-28 12:10:23 -0400

    Ajuste de fontes e assets

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,10 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Epilogue } from "next/font/google";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+const epilogue = Epilogue({ subsets: ["latin"],
+  weight: ['500','600','700']
+ });
 
 export const metadata: Metadata = {
   title: "LandingPage",
@@ -16,7 +18,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pt-br">
-      <body className={inter.className}>{children}</body>
+      <body className={epilogue.className}>{children}</body>
     </html>
   );
 }
